3bd8eff632aa2e3322bae74ed29754e4d78b77bc
Chad Fegley <chadfegley7@gmail.com>
2017-07-13 11:49:40 -0700

    Initial Commit

---
+++ Gone With The Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gone With The Weather
+//
+//  Created by Chad Fegley on 7/13/17.
+//  Copyright © 2017 Chad Fegley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
